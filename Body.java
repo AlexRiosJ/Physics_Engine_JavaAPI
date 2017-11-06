65368a7530a94a04d0e3303b7b66e6ad5ed29b58
AlfredoRdgz <33364219+AlfredoRdgz@users.noreply.github.com>
2017-11-06 10:33:12 -0600

    Update Body.java

--- Body.java
+++ Body.java
@@ -5,6 +5,5 @@ public class Body extends Particle{
 	
 	Body(Vec2D pos, Vec2D vel, Vec2D acc, Vec2D f, double damp, double mass) {
 		super(pos, vel, acc, f, damp, mass);
-	}
-
+ADSFALKDSJFLASKDJFLASKDJFLKASJDLKSADF
 }
