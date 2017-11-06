4d5b10a317b9e1106cd217561681574f3c7929ab
Alejandro Ríos Jasso <33302357+AlexRiosJ@users.noreply.github.com>
2017-11-06 10:35:26 -0600

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,20 +1,18 @@
 package com.rad.vf;
 
 public class Contact {
-	Body[] b=new Body[2];
+	Body[] b = new Body[2];
 	
 	public boolean testCircleOverlap(Body p1, Body p2) {
-		double difference=Vec2D.distanceFromSquared(p1.getPosition(), p2.getPosition());
-		double distance=Math.pow(p1.radius+p2.radius,2);
-		if(difference<distance)
+		double difference = Vec2D.distanceFromSquared(p1.getPosition(), p2.getPosition());
+		double distance=Math.pow(p1.radius + p2.radius, 2);
+		if(difference < distance)
 			return true;
 		return false;
 	}
 	
-	public boolean testPolygonOverlap(Body b1,Body b2){
-		
+	public boolean testPolygonOverlap(Body b1, Body b2){
 		return false;
 	}
-	
-	
+
 }
