09240523b30130f95aa2f37710ac6ccc0bf0ee42
Alejandro Ríos Jasso <33302357+AlexRiosJ@users.noreply.github.com>
2017-11-06 10:34:41 -0600

    Update Body.java

--- Body.java
+++ Body.java
@@ -1,9 +1,9 @@
 package com.rad.vf;
 public class Body extends Particle{
 
-	double radius,height,mass,coefficientOfRestitution;
+	double radius, height, mass, coefficientOfRestitution;
 	
-	Body(Vec2D pos, Vec2D vel, Vec2D acc, Vec2D f, double damp, double mass) {
-		super(pos, vel, acc, f, damp, mass);
+	Body(Vec2D position, Vec2D velocity, Vec2D accel, Vec2D force, double damp, double mass) {
+		super(position, velocity, accel, force, damp, mass);
 	}
 }
