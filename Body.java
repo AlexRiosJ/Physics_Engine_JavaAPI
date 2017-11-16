94ed6a0c9e9a42ac8548ff9bf96b48f1f552f1cc
AlfredoRdgz <33364219+AlfredoRdgz@users.noreply.github.com>
2017-11-15 20:39:52 -0600

    Update Body.java

--- Body.java
+++ Body.java
@@ -1,9 +1,50 @@
 package com.rad.vf;
+
 public class Body extends Particle {
 
-	double radius, height, mass, coefficientOfRestitution;
+	public double radius, height, mass, coefficientOfRestitution;
 	
-	Body(Vec2D position, Vec2D velocity, Vec2D accel, Vec2D force, double damp, double mass) {
+	public Body(double mass,double radius,double coefficient){
+		this(null, null, null, null, 0, mass);
+		this.mass=mass;
+		this.radius=radius;
+		this.coefficientOfRestitution=coefficient;
+	}
+	
+	public Body(Vec2D position, Vec2D velocity, Vec2D accel, Vec2D force, double damp, double mass) {
 		super(position, velocity, accel, force, damp, mass);
 	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+
+	public double getCoefficientOfRestitution() {
+		return coefficientOfRestitution;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+
+	public void setCoefficientOfRestitution(double coefficientOfRestitution) {
+		this.coefficientOfRestitution = coefficientOfRestitution;
+	}
+	
 }
