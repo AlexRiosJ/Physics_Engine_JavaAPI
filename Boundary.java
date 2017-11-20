fb3cba491fc76890e59ab5dfec044d453d24bf2e
AlfredoRdgz <33364219+AlfredoRdgz@users.noreply.github.com>
2017-11-20 12:14:58 -0600

    Add files via upload

---
+++ Boundary.java
@@ -0,0 +1,11 @@
+package com.rad.vf;
+
+public class Boundary {
+	double top,left,right,bottom;
+	public Boundary(double top, double left,double right, double bottom) {
+		this.top=top;
+		this.left=left;
+		this.right=right;
+		this.bottom=bottom;
+	}
+}
